6040aaa1265f8a0156966939fea8286abceb655b
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2022-04-15 23:36:16 -0400

    2da etapa de agregado de firebase

---
+++ src/Components/ItemList/ItemList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ItemList = () => {
+    return(
+        <div>
+          
+            return(
+                <div>
+                    <h1>hola</h1>
+                </div>
+            )
+        
+        </div>
+    )
+}
+
+export default ItemList;
--- src/Components/ItemList/ItemListContainer.js
+++ src/Components/ItemList/ItemListContainer.js
@@ -1,19 +1,12 @@
-import { collection, query } from 'firebase/firestore';
 import React, {useState, useEffect} from 'react';
 
-// Firebase - Firestore
-import {db} from '../../Firebase/firebaseConfig';
-
 const ItemListContainer = () => {
-    const [AlbumsData, setAlbumsData] = useState([]);
 
-    useEffect ( () => {
-        const getsProducts = async () => {
-            const q = query(collection(db, 'Shoes'));
-            const querySnapshot = await getsDocs(q);
-            querySnapshot.forEach()
-        }
-    },[]);
+    return(
+        <div>
+            <h2>hola</h2>
+        </div>
+    )
 }
 
 export default ItemListContainer;
